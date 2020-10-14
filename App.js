e58bb00d262dfd6a733c6c81328cb148b8d88c34
Hassan Kanj <contact@hassankanj.com>
2020-10-14 14:36:09 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { View, Text, Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style = {{ flex:1, alignItems: 'center', justifyContent: 'center'  }}>
+      <Text>Home Screen</Text>
+      <Button title="Go to Details"
+      onPress = { () => navigation.navigate('Details', {
+        itemId: 86,
+        otherParam: 'anything you want here'
+      }) } />
+    </View>
+  );
+}
+function DetailsScreen({ navigation, route }) {
+  const { itemId, otherParam } = route.params;
+  return (
+    <View style = {{ flex:1, alignItems: 'center', justifyContent: 'center'  }}>
+      <Text>Details Screen</Text>
+      <Text>Item id: { JSON.stringify(itemId) }</Text>
+      <Text>otherParam: { JSON.stringify(otherParam) }</Text>
+      <Button title="Go to Details... again"
+      onPress = { () => navigation.push('Details', {
+        itemId: Math.floor(Math.random() * 100),
+      }) } />
+      <Button title="Go back" onPress = { () => navigation.goBack() } />
+   </View>
+  );
+}
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview'}} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
